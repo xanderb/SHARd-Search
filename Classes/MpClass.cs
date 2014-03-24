2bff35e9c8baa2bbd1491826ec10d3ebd58fe38e
XanderB <main@xanderb.ru>
2014-03-24 06:59:48 +0400

    Merge

--- Classes/MpClass.cs
+++ Classes/MpClass.cs
@@ -8,11 +8,12 @@ namespace CCsearch
 {
     public class MpClass
     {
-        public bool IsSelect;
-        public string MpName;
-        public string FormaName;
-        public string Description1;
-        public string Description2;
+        public int Index { get; set; }
+        public Nullable<bool> Selected { get; set; }
+        public string MpName { get; set; }
+        public string FormaName { get; set; }
+        public string Description1 { get; set; }
+        public string Description2 { get; set; }
         int MpnId;
         int MpId;
         int FormaId;
@@ -26,12 +27,25 @@ namespace CCsearch
             this.FormaName = FormaName;
             this.Description1 = Description1;
             this.Description2 = Description2;
-            this.IsSelect = IsSelect;
+            this.Selected = IsSelect;
         }
 
         public override string ToString()
         {
             return this.MpName+" "+this.FormaName+" "+this.Description1+" "+this.Description2;
+        }
+
+        public int GetMpId()
+        {
+            return this.MpId;
+        }
+        public int GetMpnId() 
+        {
+            return this.MpnId;
+        }
+        public int GetFormaId()
+        {
+            return this.FormaId;
         }
     }
 }
